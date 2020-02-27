34e1cdc55e035f852479805f906f71280cf91cdd
Nick <nrwade0@gmail.com>
2020-02-27 17:39:21 -0500

    Image reading test script, waiting on downloads for OpenCV

---
+++ blackjack/read_image.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Project Euler #1
+Multiples of 3 and 5
+
+Problem 1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+    
+    
+    
+import numpy as np
+import cv2
+
+# Load an color image
+img = cv2.imread('img008.jpg')
+
+cv2.imshow('test',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
